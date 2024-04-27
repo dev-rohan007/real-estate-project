24281b542d8f9fe460f933dc712a99a24e6ce5b0
dev-rohan007 <rohandas70443@gmail.com>
2024-04-27 21:18:08 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+        sm:"0.75rem",
+        base:"0.875",
+        md:"1rem",
+        lg:"1.25rem",
+        xl:"1.5rem",
+        "2xl":"1.75rem",
+        "3xl":"2.25rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
