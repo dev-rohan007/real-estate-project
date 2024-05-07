e6bc9f5a5d17feec4e9e09955b3569ce4f24f8f8
dev-rohan007 <rohandas70443@gmail.com>
2024-05-07 11:51:05 +0530

    modified code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,24 @@ export default {
     extend: {
       fontSize:{
         sm:"0.75rem",
-        base:"0.875",
+        base:"0.875rem",
         md:"1rem",
         lg:"1.25rem",
         xl:"1.5rem",
         "2xl":"1.75rem",
-        "3xl":"2.25rem"
+        "3xl":"2.25rem",
+        "4xl":"3rem",
+        "5xl":"4rem"
+      },
+
+      backgroundImage: {
+        'team-bg-image': "url('/public/teams/team.png')",
+        
       }
     },
   },
   plugins: [],
 }
+
+
 
